d4aa7d89b59feb87791806a7791367c58a7d9cf2
Jonas Reitmann <jonas@reitmann.me>
2017-06-11 22:56:20 +0200

    Initial commit for supporting multiple dash buttons

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').load();
+const config = require('dotenv').load().parsed;
 const dashButton = require('node-dash-button');
 const hue = require("node-hue-api");
 
@@ -10,24 +10,43 @@ const lightState = hue.lightState;
 const api = new HueApi(hueIp, hueUsername);
 const state = lightState.create();
 
-const dashMacAddress = process.env.DASHMACADDRESS;
-const dash = dashButton(dashMacAddress);
+const dashButtonConfig = getDashButtonConfig();
+const dashButtonsMacAddresses = getDashButtonMacAddressesForConfig(dashButtonConfig);
+const dash = dashButton(dashButtonsMacAddresses);
 
 dash.on("detected", dashId => {
     console.log("Button press detected", dashId);
+    const groupName = getGroupForDashButtonMacAddress(dashId);
 
-    api.lights().then(result => {
-        const lights = result.lights;
+    api.groups().then(groups => {
+        const group = groups.find(group => group.name === groupName);
+        const groupLightIds = group.lights;
 
-        lights.forEach(light => {
-            const isOn = light.state.on;
+        groupLightIds.forEach(groupLightId => {
+            api.lightStatus(groupLightId).then(lightStatus => {
+                const isOn = lightStatus.state.on;
 
-            if (isOn) {
-                api.setLightState(light.id, state.off()).done();
-            } else {
-                api.setLightState(light.id, state.on()).done();
-            }
+                if (isOn) {
+                    api.setLightState(groupLightId, state.off()).done();
+                } else {
+                    api.setLightState(groupLightId, state.on()).done();
+                }
+            })
         });
     });
-
 });
+
+function getDashButtonMacAddressesForConfig(dashButtonConfig) {
+    return Object.keys(dashButtonConfig).map(key => dashButtonConfig[key]);
+}
+
+function getDashButtonConfig() {
+    const newObject = {};
+    Object.keys(config).filter(key => key.startsWith("DASH_MAC_ADDRESS_")).map(key => newObject[key] = config[key]);
+    return newObject;
+}
+
+function getGroupForDashButtonMacAddress(dashButtonMacAddress) {
+    const name = Object.keys(dashButtonConfig).find(key => dashButtonConfig[key] === dashButtonMacAddress);
+    return name.split("_").pop();
+}
